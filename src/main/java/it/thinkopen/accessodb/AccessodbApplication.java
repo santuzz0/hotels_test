4f6734c53f1f4c0d45a936c12f028b7d4642132b
santuzz0 <santo.lagana03@gmail.com>
2019-05-27 12:13:27 +0200

    first commit

---
+++ src/main/java/it/thinkopen/accessodb/AccessodbApplication.java
@@ -0,0 +1,13 @@
+package it.thinkopen.accessodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessodbApplication.class, args);
+    }
+
+}
